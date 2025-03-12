be1a7fc032d2a815d41b931f30dcb95c146f56fd
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2025-03-12 14:37:00 -0400

    Updated GameZone

--- src/pages/GameZone/GameZone.vue
+++ src/pages/GameZone/GameZone.vue
@@ -2,90 +2,25 @@
 import Header from "../../components/Header/Header.vue";
 import GameZoneList from "../GameZone/GameZoneList/GameZoneList.vue";
 import GameProgress from "../GameZone/GameProgress/GameProgress.vue";
-import { ref, onMounted } from "vue";
-import { useRouter } from "vue-router";
+import { ref } from "vue";
 
-const router = useRouter();
 const currentPage = ref(1);
-const userSession = ref(null);
-
-// Check if user is logged in on mount
-onMounted(() => {
-    const session = localStorage.getItem("audemyUserSession");
-    if (session) {
-        userSession.value = JSON.parse(session);
-    }
-});
 
 const changeCurrentPage = (page) => {
     currentPage.value = page;
 };
-
-// Logout function
-const logout = () => {
-    localStorage.removeItem("audemyUserSession");
-    userSession.value = null;
-    console.log("Logged out");
-    router.push("/"); // Redirect to login page
-};
-
-// Redirect to login page for manual login
-const login = () => {
-    router.push("/login");
-};
 </script>
-<template>
-  <div
-    class="relative bg-white h-full overflow-x-hidden flex flex-col justify-center"
-  >
-    <div class="px-20">
-      <Header
-        :textColor="'text-black'"
-        :logoPath="'/assets/images/header/header-logo-2.png'"
-      />
-    </div>
-    <div class="w-full pt-10 flex align-center justify-center">
-      <div class="w-10/12">
-        <h2 class="font-poppins text-black text-[40px] mobile:text-[25px] M-0">
-          Play and learn with us!
-        </h2>
-        <div class="w-full flex gap-10 mb-10">
-          <button
-            @click="changeCurrentPage(1)"
-            :class="
-              currentPage == 1
-                ? 'text-[#087BB4] font-semibold'
-                : 'text-[#6E777C]'
-            "
-            class="font-poppins mobile:text-[14px]"
-          >
-            Language Lab
-          </button>
-          <button
-            @click="changeCurrentPage(2)"
-            :class="
-              currentPage == 2
-                ? 'text-[#087BB4] font-semibold'
-                : 'text-[#6E777C]'
-            "
-            class="font-poppins mobile:text-[14px]"
-          >
-            Math Magic
-          </button>
-          <!--button
-            @click="changeCurrentPage(3)"
-            :class="
-              currentPage == 3
-                ? 'text-[#087BB4] font-semibold'
-                : 'text-[#6E777C]'
-            "
-            class="font-poppins mobile:text-[14px]"
-          >
-            My Progress
-          </button-->
-        </div>
 
-        <!-- Main Game Zone Section -->
+<template>
+    <div
+        class="relative bg-white h-full overflow-x-hidden flex flex-col justify-center"
+    >
+        <div class="px-20">
+            <Header
+                :textColor="'text-black'"
+                :logoPath="'/assets/images/header/header-logo-2.png'"
+            />
+        </div>
         <div class="w-full pt-10 flex align-center justify-center">
             <div class="w-10/12">
                 <h2
@@ -116,17 +51,17 @@ const login = () => {
                     >
                         Math Magic
                     </button>
-                    <button
-                        @click="changeCurrentPage(3)"
-                        :class="
-                            currentPage == 3
-                                ? 'text-[#087BB4] font-semibold'
-                                : 'text-[#6E777C]'
-                        "
-                        class="font-poppins mobile:text-[14px]"
-                    >
-                        My Progress
-                    </button>
+                    <!--button
+            @click="changeCurrentPage(3)"
+            :class="
+              currentPage == 3
+                ? 'text-[#087BB4] font-semibold'
+                : 'text-[#6E777C]'
+            "
+            class="font-poppins mobile:text-[14px]"
+          >
+            My Progress
+          </button-->
                 </div>
                 <div>
                     <div v-if="currentPage != 3">
